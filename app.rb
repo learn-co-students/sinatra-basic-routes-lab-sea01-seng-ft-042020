02e7bedbcdacd8ae6b2578c9cc24ea0df7dcf244
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-05-10 21:11:26 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get "/name" do
+        "My name is Mat"
+    end
+
+    get "/hometown" do
+        "My hometown is Leonardtown"
+    end
+
+    get "/favorite-song" do
+        "My favorite song is 'It's raining men'"
+    end
+
 end
