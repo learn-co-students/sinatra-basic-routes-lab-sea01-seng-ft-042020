f830e004cecc834f2f740395eaf417f15ba582dd
Trevor <tlow94@uw.edu>
2020-05-10 18:23:42 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Trevor"
+    end
+
+    get '/hometown' do
+        "My hometown is Bellevue"
+    end
+    get '/favorite-song' do
+        "My favorite song is Savage"
+    end
 end
